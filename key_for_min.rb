9c108d747b41ba32c4a09e42655dc5791ab3b01b
Sean Febo <seanfebo@techie.com>
2018-04-08 03:19:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(name_hash)
   answer = nil
   compare = Float::INFINITY
   name_hash.each do |name, num|
-    if num < minimum
+    if num < compare
       compare = num
       answer = name
     end
